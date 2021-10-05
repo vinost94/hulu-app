dd63b2be78523730de62aa3b8e34d4a8626cf72c
Stylish Thamizhan <vino2420@gmail.com>
2021-10-05 10:11:11 +0530

    initial commit

---
+++ components/Header.tsx
@@ -0,0 +1,33 @@
+import { 
+    BadgeCheckIcon,
+    CollectionIcon,
+    HomeIcon,
+    LightBulbIcon,
+    SearchIcon,
+    UserIcon,
+} from "@heroicons/react/outline";
+import Image from "next/Image";
+import HeaderItem from './HeaderItem';
+
+function Header() {
+    return (
+        <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+            <div className="flex flex-grow justify-evenly max-w-2xl">
+                <HeaderItem title="Home" Icon={HomeIcon} />
+                <HeaderItem title="Trending" Icon={LightBulbIcon} />
+                <HeaderItem title="Verified" Icon={BadgeCheckIcon} />
+                <HeaderItem title="Collections" Icon={CollectionIcon} />
+                <HeaderItem title="Search" Icon={SearchIcon} />
+                <HeaderItem title="Account" Icon={UserIcon} />
+            </div>
+            <Image 
+                className="object-contain"
+                src="/logo/Hulu-Logo.png"
+                width={90}
+                height={50}
+             />
+        </header>
+    )
+}
+
+export default Header
---
+++ components/HeaderItem.tsx
@@ -0,0 +1,10 @@
+function HeaderItem({ title, Icon }) {
+    return (
+        <div className="flex flex-col items-center cursor-pointer group w-12 sm:w-20 hover:text-white">
+            <Icon className="h-6 mb-1 group-hover:animate-bounce" />
+            <p className="tracking-widest capitalize opacity-0 group-hover:opacity-100">{title}</p>
+        </div>
+    )
+}
+
+export default HeaderItem
